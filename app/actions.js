264f6623a856bae5826709e2f8db2e9f80a13b4d
Joseph Doty <bigredjoe@users.noreply.github.com>
2018-01-20 20:58:26 -0600

    Adding fading verse

--- app/actions.js
+++ app/actions.js
@@ -3,6 +3,7 @@
 module.exports = {
   REFERENCE_CHANGE: 'REFERENCE_CHANGE',
   RETRIEVING_VERSE: 'RETRIEVING_VERSE',
+  FADE_VERSE: 'FADE_VERSE',
   
   referenceChange: function(reference) {
     return {
@@ -15,6 +16,13 @@ module.exports = {
     return {
       type: this.RETREIVING_VERSE,
       verse: verse
+    }
+  },
+  
+  fadeVerse: function(opacity) {
+    return {
+      type: this.FADE_VERSE,
+      opacity: opacity
     }
   }
   
