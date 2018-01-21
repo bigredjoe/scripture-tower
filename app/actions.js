4e8df4a897d91e3023fe157af4da474cf47e1edb
Joseph Doty <bigredjoe@users.noreply.github.com>
2018-01-21 16:33:08 -0600

    Adding Fade verse

--- app/actions.js
+++ app/actions.js
@@ -3,6 +3,7 @@
 module.exports = {
   REFERENCE_CHANGE: 'REFERENCE_CHANGE',
   RETRIEVING_VERSE: 'RETRIEVING_VERSE',
+  RETRIEVED_VERSE: 'RETRIEVED_VERSE',
   FADE_VERSE: 'FADE_VERSE',
   
   referenceChange: function(reference) {
@@ -11,11 +12,18 @@ module.exports = {
       reference: reference
     }
   },
+  
+  referenceRetrieved: function(verse) {
+    return {
+      type: this.RETRIEVED_VERSE,
+      verse: verse
+    }
+  },
 
-  retreivingVerse: function(verse) {
+  retreivingVerse: function(reference) {
     return {
       type: this.RETREIVING_VERSE,
-      verse: verse
+      reference: references
     }
   },
   
--- app/reducers.js
+++ app/reducers.js
@@ -1,19 +1,23 @@
 /* reducers */
 
-const { UPVOTE, DOWNVOTE } = require('./actions');
+const { REFERENCE_CHANGE, RETRIEVING_VERSE, FADE_VERSE } = require('./actions');
 
 function votes(state = [], action) {
   switch (action.type) {
-    case UPVOTE: 
+    case REFERENCE_CHANGE: 
       return Object.assign({}, state, {
         voteScore: ( state.voteScore ) ? state.voteScore + 1 : 1,
         voteCount: ( state.voteCount ) ? state.voteCount + 1 : 1
       });
-    case DOWNVOTE:
+    case RETRIEVING_VERSE:
        return Object.assign({}, state, {
         voteScore: ( state.voteScore ) ? state.voteScore - 1 : -1,
         voteCount: ( state.voteCount ) ? state.voteCount + 1 : 1
       });
+    case FADE_VERSE:
+    	return Object.assign({}, state, {
+    		verseOpacity: action.opacity 
+    	});
     default:
       return state;
   }
